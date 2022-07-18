b05a8f0d15f00cabd25fcc0fd6e21af0a940fcf1
Egemen Ulucam <81033171+AgemennoN@users.noreply.github.com>
2022-07-19 02:25:28 +0300

    Play again button is added
    Play again button is added on statistics page.

--- TriviaGame/Assets/Scripts/StatisticsPanel.cs
+++ TriviaGame/Assets/Scripts/StatisticsPanel.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine;
 using TMPro;
 
 
@@ -10,6 +11,7 @@ public class StatisticsPanel : MonoBehaviour
     [SerializeField] private GameObject ResultPanel;
     [SerializeField] private TextMeshProUGUI ScoreTxt;
     [SerializeField] private TextMeshProUGUI CategoryStatsTextPrefab;
+    [SerializeField] private Button PlayAgainBtn;
     private GameManager gameManager;
     private int CorrectAnswers;
     private List<TCategory> diffrentCategories;
@@ -57,7 +59,6 @@ public class StatisticsPanel : MonoBehaviour
         public string name;
         public int totalAnswered;
         public int correctlyAnswered;
-
         public TCategory(QuestionPanel QP)
         {
             name = QP.question.category;
@@ -66,7 +67,6 @@ public class StatisticsPanel : MonoBehaviour
                 correctlyAnswered = 1;
             else
                 correctlyAnswered = 0;
-
         }
     }
 
@@ -81,30 +81,61 @@ public class StatisticsPanel : MonoBehaviour
 
     private void DisplayResults()
     {
-
-        if (diffrentCategories.Count > 1)
+        string tempText;
+        if (diffrentCategories.Count > 1)   // That means Game Played with random categories
         {
             ScoreTxt.text = CorrectAnswers + " / " + StaticGameInfo.totalQuestionNumber;
             foreach (TCategory category in diffrentCategories)
             {
                 TextMeshProUGUI CategoryTxt = Instantiate(CategoryStatsTextPrefab, new Vector3(0, 0, 0), Quaternion.Euler(0, 0, 0), ResultPanel.transform);
-                string text = category.name + ": " + category.correctlyAnswered + " / " + category.totalAnswered;
-                CategoryTxt.text = text;
-                //GameObject CategoryTxt = new GameObject(category.name, typeof(Text), typeof(LayoutElement));
-                //CategoryTxt.transform.parent = ResultPanel.transform;
-                //string text = category.name + ": " + category.correctlyAnswered + " / " + category.totalAnswered;
-                //CategoryTxt.GetComponent<Text>().text = text;
-                //CategoryTxt.GetComponent<Text>().font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
-
+                tempText = category.name + ": " + category.correctlyAnswered + " / " + category.totalAnswered;
+                CategoryTxt.text = tempText;
             }
+
+            PlayAgainBtn.onClick.AddListener(PlayAgainWithRandomQuestions);
+            tempText = "Play Again with Random Categories";
+            PlayAgainBtn.GetComponentInChildren<TextMeshProUGUI>().text = tempText;
         }
         else
         {
             ScoreTxt.text = "In Category: " + StaticGameInfo.selectedCategory.name + "\n";
             ScoreTxt.text += CorrectAnswers + " / " + StaticGameInfo.totalQuestionNumber;
 
+            PlayAgainBtn.onClick.AddListener(PlayAgainWithSelectedCategory);
+            tempText = "Play Again with Category: " + StaticGameInfo.selectedCategory.name;
+            PlayAgainBtn.GetComponentInChildren<TextMeshProUGUI>().text = tempText;
         }
 
+    }
+
+    private void PlayAgainWithSelectedCategory()
+    {
+        StaticGameInfo.questionRequest = APIHelper.ApiFetchQuestionsByCategory(StaticGameInfo.selectedCategory);
+        if (StaticGameInfo.questionRequest.response_code == 0)
+        {
+            StaticGameInfo.totalQuestionNumber = StaticGameInfo.questionRequest.results.Length;
+            SceneManager.LoadScene("GameScene");
+        }
+        else
+        {
+            Debug.Log("response_code = " + StaticGameInfo.questionRequest.response_code);
+        }
+    }
+
+    private void PlayAgainWithRandomQuestions()
+    {
+        StaticGameInfo.questionRequest = APIHelper.ApiFetchRandomQuestions();
+
+        if (StaticGameInfo.questionRequest.response_code == 0)
+        {
+            StaticGameInfo.totalQuestionNumber = StaticGameInfo.questionRequest.results.Length;
+            SceneManager.LoadScene("GameScene");
+
+        }
+        else
+        {
+            Debug.Log("response_code = " + StaticGameInfo.questionRequest.response_code);
+        }
     }
 
 }
