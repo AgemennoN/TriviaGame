0e682569f4a8c80379a3b2996e87548949f4a7c7
Egemen Ulucam <81033171+AgemennoN@users.noreply.github.com>
2022-07-19 00:04:16 +0300

    SearchBar for Categorie Search is added
    Categories can be searched in category screen now

--- TriviaGame/Assets/Scripts/CategoryScreenHandler.cs
+++ TriviaGame/Assets/Scripts/CategoryScreenHandler.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -8,38 +10,71 @@ public class CategoryScreenHandler : MonoBehaviour
     [SerializeField] private GameObject buttonPrefab;
     [SerializeField] private GameObject MainMenuScreen;
     [SerializeField] private GameObject ScrollViewContent;
+    [SerializeField] private TMP_InputField InputCategory;
     public Category[] categories;
     public GameObject[] categoryButtons;
+    private List<string> categoryNames;
 
     void Awake()
     {
         SetCategoryButtons();
+
+        EnableSearchedCategories();
+
     }
 
-    public void SetCategoryButtons()
+    private void SetCategoryButtons()
     {
         categories = APIHelper.ApiFetchCategories().trivia_categories;
+        categoryNames = new List<string>();
         categoryButtons = new GameObject[categories.Length];
 
         for (int i = 0; i < categories.Length; i++)
         {
             categoryButtons[i] = Instantiate(buttonPrefab, new Vector3(0, 0, 0), Quaternion.Euler(0, 0, 0), ScrollViewContent.transform);
-            
             categoryButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = categories[i].name;
+            categoryNames.Add(categories[i].name.ToLower());
+
+
 
             Category category = categories[i];  // To use the index with delegate
-
             Button button = categoryButtons[i].GetComponent<Button>();
             button.onClick.AddListener(delegate { SelectCategory(category); });
 
+            categoryButtons[i].gameObject.SetActive(false);
         }
+    }
+
+    public void EnableSearchedCategories()
+    {
+        if (InputCategory.text.Trim() == "" || InputCategory.text == null)
+        {
+            for (int i = 0; i < categoryNames.Count; i++)
+            {
+                categoryButtons[i].SetActive(true);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < categoryNames.Count; i++)
+            {
+                if (categoryNames[i].Contains(InputCategory.text.ToLower().Trim()))
+                    categoryButtons[i].SetActive(true);
+                else
+                    categoryButtons[i].SetActive(false);
+            }
+        }
+    }
+
+    public void ClearSearchbar()
+    {
+        InputCategory.text = "";
     }
 
     public void SelectCategory(Category category)
     {
         StaticGameInfo.selectedCategory = category;
-        
-        Debug.Log(category.name);
+
 
         StaticGameInfo.questionRequest = APIHelper.ApiFetchQuestionsByCategory(category);
         if (StaticGameInfo.questionRequest.response_code == 0)
